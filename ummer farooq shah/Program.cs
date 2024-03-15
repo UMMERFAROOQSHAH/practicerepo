66614cfdd412508fca99ef6315d1069c3cb06c54
ummerfarooqshah <suhaanumer07@gmail.com>
2024-03-15 11:49:23 +0530

    test11

---
+++ ummer farooq shah/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ummer_farooq_shah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UMMER FAROOQ SHAH");
+            Console.WriteLine("ROLL NMBR:21770022");
+            int a = 55;
+            int b = a;
+            b = 90;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            int[] arr = { 1, 2, 3, 4 };
+            int[] arr2 = arr;
+            arr2[0] = 10;
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+        }
+    }
+}
